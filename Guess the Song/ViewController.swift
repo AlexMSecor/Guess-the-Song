0312faa9d51aada13e0600804b7421b808253e7a
alexmsecor <alex.secor16@gmail.com>
2025-02-13 15:28:15 -0500

    UI rework: New logo, main screen, and background color (second commit)

--- Guess the Song/ViewController.swift
+++ Guess the Song/ViewController.swift
@@ -6,9 +6,17 @@ class ViewController: UIViewController {
     
     var fetchedSongs: [String] = []
     let songFuncs = SongFuncs()
+    // Background view that will contain the falling notes
+    private let backgroundView = UIView()
+    // For the background
+    let musicNotes = ["🎵", "🎶", "🎼", "?"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Background of the main screen
+        setupBackgroundView()
+        startMusicNoteAnimation()
         
         // For testing purposes only
         //songFuncs.deleteAllSongs()
@@ -72,4 +80,57 @@ class ViewController: UIViewController {
         }
     }
 
+    func startMusicNoteAnimation() {
+        Timer.scheduledTimer(withTimeInterval: 0.25, repeats: true) { _ in
+            self.createFallingNote()
+        }
+    }
+
+    private func setupBackgroundView() {
+        // Creates a background that covers the entire view, is transparent, and is sent to the back
+        backgroundView.frame = view.bounds
+        backgroundView.backgroundColor = .clear
+        view.addSubview(backgroundView)
+        view.sendSubviewToBack(backgroundView)
+    }
+    
+    func createFallingNote() {
+        let screenWidth = UIScreen.main.bounds.width
+        // Random X position
+        let startX = CGFloat.random(in: 0...screenWidth)
+        // Random music note
+        let emoji = musicNotes.randomElement() ?? "🎵"
+        
+        // Assign a random font size
+        let fontSize = CGFloat.random(in: 20...60)
+        // Increase size to allow for rotation (otherwise the user will experience clipping)
+        let labelSize = fontSize * 2
+        
+        let noteLabel = UILabel()
+        noteLabel.text = emoji
+        noteLabel.font = UIFont.systemFont(ofSize: fontSize)
+        // Increase label frame size to accommodate rotation
+        noteLabel.frame = CGRect(x: startX, y: -labelSize, width: labelSize, height: labelSize)
+        // Center the emoji inside
+        noteLabel.textAlignment = .center
+        // No background (prevents visual artifacts)
+        noteLabel.backgroundColor = .clear
+        // Assign a random tilt (so they all won't look the same)
+        noteLabel.transform = CGAffineTransform(rotationAngle: CGFloat.random(in: -0.5...0.5))
+        
+        // Adding the note to the backgronudView
+        backgroundView.addSubview(noteLabel)
+        
+        // Assign a random fall speed
+        let duration = Double.random(in: 4...8)
+        
+        // Assign the falling animation
+        UIView.animate(withDuration: duration, delay: 0, options: .curveLinear, animations: {
+            // Move it down
+            noteLabel.frame.origin.y = UIScreen.main.bounds.height + labelSize
+        }, completion: { _ in
+            // Remove it after animation completes
+            noteLabel.removeFromSuperview()
+        })
+    }
 }
